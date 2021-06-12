2ea786a3891ebece1ee3c1acfb4627837e4e4f9b
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-06-12 23:52:13 +0900

    Remove Extra code

--- public/ts/canvas.ts
+++ public/ts/canvas.ts
@@ -1,9 +1,8 @@
-import init, { start } from "../pkg/wasm_canvas.js";
+import init from "../pkg/wasm_canvas.js";
 
 async function run() {
   const wasm = await fetch("../public/pkg/wasm_canvas_bg.wasm");
   await init(wasm);
-  start();
 }
 
 run();
