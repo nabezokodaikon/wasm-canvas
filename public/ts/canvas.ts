a4b73c8e866be09c9c919c026ca4ab39b5908057
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-06-14 23:26:33 +0900

    Remove comment line

--- public/ts/canvas.ts
+++ public/ts/canvas.ts
@@ -4,17 +4,6 @@ import init, {
 } from "../pkg/wasm_canvas.js";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-// function sleep(ms: number) {
-  // return new Promise(resolve => setTimeout(resolve, ms))
-// }
-
-// async function sleep(ms: number) {
-  // await setTimeout(ms);
-// }
-
-// function sleep(ms: number): Promise<void> {
-  // return new Promise<void>(resolve => setTimeout(resolve, ms))
-// }
 
 async function run() {
   const wasm = await fetch("public/pkg/wasm_canvas_bg.wasm");
