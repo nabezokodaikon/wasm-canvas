7f31640be40c5375ab40c99c72cd272430ca0bbb
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-06-14 10:29:17 +0900

    Modify fetch path

--- public/ts/canvas.ts
+++ public/ts/canvas.ts
@@ -1,7 +1,7 @@
 import init, { console_log } from "../pkg/wasm_canvas.js";
 
 async function run() {
-  const wasm = await fetch("../public/pkg/wasm_canvas_bg.wasm");
+  const wasm = await fetch("./public/pkg/wasm_canvas_bg.wasm");
   await init(wasm);
   console_log("Start!");
 }
